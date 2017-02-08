bc2817812e6751f4208cbff199fbee2c3727124c
Joshua Licht <jlicht@gmail.com>
2017-02-07 16:24:18 -0800

    fixed some stuff

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ hbs.registerPartials(__dirname + '/views/partials');
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 
-
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,19 @@ var router = express.Router();
 
 
 var downloads = [
-        {pagetitle:'title', description:'the description'},
-        {pagetitle:'title', description:'the description'},
-        {pagetitle:'title', description:'the description'}
+        {pagetitle:'title1', description:'the description1'},
+        {pagetitle:'title2', description:'the description2'},
+        {pagetitle:'title3', description:'the description3'}
     ]
 
+var artists = [
+        {pagetitle:'artist1', description:'the description1'},
+        {pagetitle:'artist2', description:'the description2'},
+        {pagetitle:'artist3', description:'the description3'}  
+]
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: downloads });
+  res.render('index', { albums: downloads, artists: artists });
 });
 
 module.exports = router;
